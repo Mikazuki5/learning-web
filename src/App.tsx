e98925d715e535a150fb2039a28dd58a31188013
Gamal Rivaldi M <gamlrvldi5@gmail.com>
2022-07-28 14:09:46 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import  LayoutContainer  from "./shared/component/layout/LayoutContainer";
+import HomeContainer from './page/home/home-container';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <LayoutContainer>
+    <HomeContainer />
+   </LayoutContainer>
   );
 }
 
